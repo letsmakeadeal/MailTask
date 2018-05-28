5ad2de6d853f53981cfea2ca95fe528a3f1aea97
Ivan Rodin <rodinivann@yandex.ru>
2018-05-28 13:01:32 +0300

    Final fixes

--- URLParser.cpp
+++ URLParser.cpp
@@ -7,37 +7,56 @@ void URLParser::Parse(const std::string& url) {
   size_t pos_of_prot_beg = url.find("http://");
   if (pos_of_prot_beg == std::string::npos) {
     throw std::runtime_error("Can't find http protocol in URL ");
-    return;
   }
   size_t pos_of_prot_end = url.find("://");
   protocol_ =
       std::string(url.begin() + pos_of_prot_beg, url.begin() + pos_of_prot_end);
 
-  size_t pos_of_host_end = url.find(':', pos_of_prot_end + 3);
+  const int http_size = 7;
 
-  if (pos_of_host_end == std::string::npos) {
-    pos_of_host_end = url.find('/', pos_of_prot_end + 3);
+  if (url.size() == 7) return;
+
+  size_t pos_of_host_end = 0;
+  if ( url[pos_of_prot_end + 3] == '[' ) {
+    size_t end_of_ipv6_host = url.find(']', pos_of_prot_end + 3);
+
+    if (end_of_ipv6_host == std::string::npos)
+      throw std::runtime_error("Ipv6 invalid host");
+
+    host_ = std::string(url.begin() + pos_of_prot_end + 4,
+                        url.begin() + end_of_ipv6_host );
+    pos_of_host_end = end_of_ipv6_host + 1;
+
+  } else {
+    pos_of_host_end = url.find(':', pos_of_prot_end + 3);
+
+    size_t pos_of_host_end_with_slash = url.find('/', pos_of_prot_end + 3);
+
+    if ((pos_of_host_end > pos_of_host_end_with_slash ||
+         pos_of_host_end == std::string::npos) &&
+        pos_of_host_end_with_slash != std::string::npos) {
+      pos_of_host_end = pos_of_host_end_with_slash;
+    }
+
+    if (pos_of_host_end == std::string::npos) {
+      pos_of_host_end = url.find('?', pos_of_prot_end + 3);
+    }
+
+    if (pos_of_host_end == std::string::npos) {
+      pos_of_host_end = url.find('#', pos_of_prot_end + 3);
+    }
+
+    if (pos_of_host_end == std::string::npos) pos_of_host_end = url.size();
+
+    host_ = std::string(url.begin() + pos_of_prot_end + 3,
+                        url.begin() + pos_of_host_end);
+
+    if (pos_of_host_end == url.size() ||
+        (pos_of_host_end == url.size() - 1 && url[pos_of_host_end] == '/')) {
+      path_ = '/';
+      return;
+    }
   }
-
-  if (pos_of_host_end == std::string::npos) {
-    pos_of_host_end = url.find('?', pos_of_prot_end + 3);
-  }
-
-  if (pos_of_host_end == std::string::npos) {
-    pos_of_host_end = url.find('#', pos_of_prot_end + 3);
-  }
-
-  if (pos_of_host_end == std::string::npos) pos_of_host_end = url.size();
-
-  host_ = std::string(url.begin() + pos_of_prot_end + 3,
-                      url.begin() + pos_of_host_end);
-
-  if (pos_of_host_end == url.size() ||
-      (pos_of_host_end == url.size() - 1 && url[pos_of_host_end] == '/')) {
-    path_ = '/';
-    return;
-  }
-
   // Parsing port
   size_t pos_of_port_end = 0;
 
